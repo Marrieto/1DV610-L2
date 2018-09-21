7abeb37466485075af45b977640a4cf266f5c8e3
Martin Lindahl <martin_lindahl9@hotmail.com>
2018-09-21 10:27:12 +0200

    Fixed the timeview showing wrong ordinal

--- model/TimeModel.php
+++ model/TimeModel.php
@@ -9,5 +9,19 @@
 		$month				= $currentTime["month"];
 		$year				= $currentTime["year"];
 
-		return $weekDay . ", the " . $mday . "th of " . $month . " " . $year . ", The time is ";
+
+
+		return $weekDay . ", the " . ordinal($mday) . " of " . $month . " " . $year . ", The time is ";
 	}
+
+	/* 	Copied from:
+			https://stackoverflow.com/questions/3109978/display-numbers-with-ordinal-suffix-in-php
+			21/09/18
+	*/
+	function ordinal($number) {
+    $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+    if ((($number % 100) >= 11) && (($number%100) <= 13))
+        return $number. 'th';
+    else
+        return $number. $ends[$number % 10];
+}
