491e193494e0c0734ea7044c383746b8cdb11f0e
Martin Lindahl <martin_lindahl9@hotmail.com>
2018-10-09 17:02:19 +0200

    Removed all the static variables in Cookies.php

--- model/Cookies.php
+++ model/Cookies.php
@@ -3,25 +3,25 @@
 class Cookies
 {
 
-    public static function setCookie($cookiename, $value)
+    public function setCookie($cookiename, $value)
     {
         setcookie($cookiename, $value, (time() + 60 * 60 * 24));
     }
 
-    public static function getCookie($cookiename)
+    public function getCookie($cookiename)
     {
         if (isset($_COOKIE[$cookiename])) {
             return $_COOKIE[$cookiename];
         }
     }
 
-    public static function removeCookie($cookiename)
+    public function removeCookie($cookiename)
     {
         setcookie($cookiename, "", (time() + 60 * 60 * 24));
         unset($_COOKIE[$cookiename]);
     }
 
-    public static function hasCookie($cookiename)
+    public function hasCookie($cookiename)
     {
         return isset($_COOKIE[$cookiename]);
     }
