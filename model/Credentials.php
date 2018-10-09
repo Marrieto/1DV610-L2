dced94b72e781d0e8fa6103b41ee57b6bf877c88
Martin Lindahl <martin_lindahl9@hotmail.com>
2018-10-09 17:00:45 +0200

    Removed all the static variables in Credentials.php

--- model/Credentials.php
+++ model/Credentials.php
@@ -6,56 +6,56 @@ A class to keep credential pairs
 class Credentials
 {
 
-    private static $username = "";
-    private static $password = "";
-    private static $passwordRepeat = "";
-    private static $cookieString = "";
-    private static $cookiePassword = "";
-    private static $statusMessage = "";
-    private static $keepLoggedIn = false;
+    private $username = "";
+    private $password = "";
+    private $passwordRepeat = "";
+    private $cookieString = "";
+    private $cookiePassword = "";
+    private $statusMessage = "";
+    private $keepLoggedIn = false;
 
     public function __construct($username, $password, $keepLoggedIn, $cookieString, $cookiePassword, $passwordRepeat, $statusMessage)
     {
-        self::$username = $username;
-        self::$password = $password;
-        self::$passwordRepeat = $passwordRepeat;
-        self::$cookieString = $cookieString;
-        self::$cookiePassword = $cookiePassword;
-        self::$keepLoggedIn = $keepLoggedIn;
-        self::$statusMessage = $statusMessage;
+        $this->username = $username;
+        $this->password = $password;
+        $this->passwordRepeat = $passwordRepeat;
+        $this->cookieString = $cookieString;
+        $this->cookiePassword = $cookiePassword;
+        $this->keepLoggedIn = $keepLoggedIn;
+        $this->statusMessage = $statusMessage;
     }
 
     public function getUsername()
     {
-        return self::$username;
+        return $this->username;
     }
     public function getPassword()
     {
-        return self::$password;
+        return $this->password;
     }
     public function getPasswordRepeat()
     {
-        return self::$passwordRepeat;
+        return $this->passwordRepeat;
     }
     public function getKeepLoggedIn()
     {
-        return self::$keepLoggedIn;
+        return $this->keepLoggedIn;
     }
     public function getCookieString()
     {
-        return self::$cookieString;
+        return $this->cookieString;
     }
     public function getCookiePassword()
     {
-        return self::$cookiePassword;
+        return $this->cookiePassword;
     }
     public function getStatusMessage()
     {
-        return self::$statusMessage;
+        return $this->statusMessage;
     }
     public function setStatusMessage($message)
     {
-        self::$statusMessage = $message;
+        $this->statusMessage = $message;
     }
 
     // Checks if the credential object is having the right format
@@ -64,14 +64,14 @@ class Credentials
         $returnMessage = new StatusMessage();
 
         // Check if username exists
-        if (!strlen(self::$username) > 0) {
+        if (!strlen($this->username) > 0) {
             $returnMessage->setMessageState(false);
             $returnMessage->setMessageString("Username is missing");
             return $returnMessage;
         }
 
         // Check if password exists
-        if (!strlen(self::$password) > 0) {
+        if (!strlen($this->password) > 0) {
             $returnMessage->setMessageState(false);
             $returnMessage->setMessageString("Password is missing");
             return $returnMessage;
